be2eb16fb15baed00e4bc621f3875be2722a0dc3
thaisavelino <t.avelino@gmail.com>
2019-07-03 15:12:07 +0200

    testing index

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     
     <title>Calendar PHP</title>
 </head>
@@ -22,11 +22,11 @@
     
     <?php
         require '../src/Date/Month.php'; /* Por enqunanto damos require aqui, depois vamos criar um autoloads */
-        //$month = new App\Date\Month(02,1988); /* chama construtor month */
+        //$month = new App\Date\Month(03,2018); /* chama construtor month */
         $month = new App\Date\Month($_GET['month'] ?? null, $_GET['year'] ?? null);
             // se tem o valor pega se não o default é null
             // Agora pode passaar pela URL -> http://localhost:8001/?month=10&year=2012
-        $start = $month->getStartingDay()->modify('last  monday');
+        $start = $month->getFirstDay()->modify('last  monday'); //qual dia começará o calendario.
 ?>
     <div class="container">
         <h1><?= $month->toString(); ?></h1>
@@ -36,21 +36,26 @@
         <table class="table col-md-12 text-center">
             <?php for ($i = 0; $i < $month->getWeeks(); $i++): ?>
                 <tr class="week-col">
-                    <?php foreach($month->weekDay as $value => $day):
-                        $date = (clone $start)->modify("+" . ($value + $i * 7) . "weekDay" );
-                    ?>
+                    <?php
+                    foreach($month->weekDay as $k => $wDay):
+                        $date = (clone $start)->modify("+" . ($k + $i * 7) . " weekDay")
+                        ?>
                     <td class="day">
-                        <?php if ($i === 0): ?>
-                            <h5><?= $day; //$day->format('d'); ?></h5>
+                        <?php if ($i === 0): //Se é primeira semana ?>
+                            <h5><?= $wDay; ?></h5>
                         <?php endif; ?>
                         <span class="<?= $month->actualMonth($date) ? 'text-primary': 'text-secondary' ; ?>">
-                        <?= $date->format('d'); ?>
-                            </span>
+                            <?= $date->format('d'); ?>
+                         </span>
                     </td>
                     <?php endforeach; ?>
                 </tr>
             <?php endfor; ?>
         </table>
-    </div>
+
+        <div class="float-right">
+            <a href="/index.php?month=<?= $month->previousMonth()->month; ?>&year=<?= $month->previousMonth()->year; ?>" class="btn btn-primary">&lt;</a>
+            <a href="/index.php?month=<?= $month->nextMonth()->month; ?>&year=<?= $month->nextMonth()->year; ?>" class="btn btn-primary">&gt;</a>        </div>
+        </div>
 </body>
 </html>
